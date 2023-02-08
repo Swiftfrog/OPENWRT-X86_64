ce67fb92cff270191c3fef106193e60e4f21f6a2
Albert <jiuzheyanglianxiwo@gmail.com>
2023-02-08 11:44:55 +0800

    Update 2102-diy2.sh

--- 2102-diy2.sh
+++ 2102-diy2.sh
@@ -16,6 +16,9 @@ sed -i 's/192.168.1.1/192.168.99.1/g' package/base-files/files/bin/config_genera
 #passwall-package 
 git clone https://github.com/xiaorouji/openwrt-passwall.git  package/passwall
 git clone -b luci https://github.com/xiaorouji/openwrt-passwall package/openwrt-passwall
+git clone https://github.com/xiaorouji/openwrt-passwall2.git package/passwall2
+#Lucky-Go
+git clone https://github.com/sirpdboy/luci-app-lucky.git package/lucky
 
 #add Lean package
 #luci-app-ipsec-vpnd
