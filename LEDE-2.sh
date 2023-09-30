8d50f0bbfd235c6e632d05d64c542324b0682f1d
Albert <jiuzheyanglianxiwo@gmail.com>
2023-09-30 12:35:38 +0800

    Update LEDE-2.sh

--- LEDE-2.sh
+++ LEDE-2.sh
@@ -11,7 +11,7 @@
 #
 
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.99.1/g' package/base-files/files/bin/config_generate
+#sed -i 's/192.168.1.1/192.168.99.1/g' package/base-files/files/bin/config_generate
 
 #passwall-package 
 #git clone https://github.com/xiaorouji/openwrt-passwall.git package/openwrt-passwall
