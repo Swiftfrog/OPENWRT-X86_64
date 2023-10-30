4e683f52712c0e3c7a38343a10de15c3648fd69d
Albert <jiuzheyanglianxiwo@gmail.com>
2023-10-30 22:01:24 +0800

    Update OPENWRT.2305.2.sh

--- OPENWRT.2305.2.sh
+++ OPENWRT.2305.2.sh
@@ -13,6 +13,9 @@
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.99.1/g' package/base-files/files/bin/config_generate
 
+# Modify BCM57810
+wget -O target/linux/x86/patches-5.4/600-bnx2x-warpcore-8727-2g5.patch https://raw.githubusercontent.com/JAMESMTL/snippets/master/bnx2x/patches/bnx2x_warpcore_8727_2_5g_sgmii_txfault.patch
+
 #update golang
 #pushd feeds/packages/lang
 #rm -rf golang && svn co https://github.com/openwrt/packages/branches/openwrt-22.03/lang/golang
