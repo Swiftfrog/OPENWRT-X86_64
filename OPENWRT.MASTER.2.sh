174d85fc0f8e5fb90f56cb99cd338747acb29e96
Albert <jiuzheyanglianxiwo@gmail.com>
2024-01-28 20:25:22 +0800

    Update OPENWRT.MASTER.2.sh

--- OPENWRT.MASTER.2.sh
+++ OPENWRT.MASTER.2.sh
@@ -13,9 +13,9 @@
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.99.1/g' package/base-files/files/bin/config_generate
 
-wget -O include/kernel-defaults.mk https://raw.githubusercontent.com/Swiftfrog/OPENWRT-X86_64/main/version/kernel-defaults.mk
-wget -O package/kernel/linux/Makefile https://raw.githubusercontent.com/Swiftfrog/OPENWRT-X86_64/main/version/Makefile
-wget https://raw.githubusercontent.com/Swiftfrog/OPENWRT-X86_64/main/version/vermagic
+#wget -O include/kernel-defaults.mk https://raw.githubusercontent.com/Swiftfrog/OPENWRT-X86_64/main/version/kernel-defaults.mk
+#wget -O package/kernel/linux/Makefile https://raw.githubusercontent.com/Swiftfrog/OPENWRT-X86_64/main/version/Makefile
+#wget https://raw.githubusercontent.com/Swiftfrog/OPENWRT-X86_64/main/version/vermagic
 
 # Modify BCM57810
 # wget -O target/linux/x86/patches-5.15/993-bnx2x_warpcore_8727_2_5g_sgmii_txfault.patch https://raw.githubusercontent.com/Swiftfrog/OPENWRT-X86_64/main/993-bnx2x_warpcore_8727_2_5g_sgmii_txfault.patch
