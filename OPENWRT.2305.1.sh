eacdf84e8dccaaeb85a85e635324ddc0eefc116f
Albert <jiuzheyanglianxiwo@gmail.com>
2024-01-28 12:00:36 +0800

    Update OPENWRT.2305.1.sh

--- OPENWRT.2305.1.sh
+++ OPENWRT.2305.1.sh
@@ -13,8 +13,8 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Modify BCM57810
-#wget -O target/linux/x86/patches-5.15/600-bnx2x-warpcore-8727-2g5.patch https://raw.githubusercontent.com/JAMESMTL/snippets/master/bnx2x/patches/bnx2x_warpcore_8727_2_5g_sgmii_txfault.patch
-wget -O target/linux/x86/patches-6.1/600-bnx2x-warpcore-8727-2g5.patch https://raw.githubusercontent.com/JAMESMTL/snippets/master/bnx2x/patches/bnx2x_warpcore_8727_2_5g_sgmii_txfault.patch
+wget -O target/linux/x86/patches-5.15/600-bnx2x-warpcore-8727-2g5.patch https://raw.githubusercontent.com/JAMESMTL/snippets/master/bnx2x/patches/bnx2x_warpcore_8727_2_5g_sgmii_txfault.patch
+#wget -O target/linux/x86/patches-6.1/600-bnx2x-warpcore-8727-2g5.patch https://raw.githubusercontent.com/JAMESMTL/snippets/master/bnx2x/patches/bnx2x_warpcore_8727_2_5g_sgmii_txfault.patch
 
 
 # 添加APP过滤模块
