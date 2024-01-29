2fd7285e75ce0f3858cc7770d2e169d992375dd4
Albert <jiuzheyanglianxiwo@gmail.com>
2024-01-29 20:22:20 +0800

    Update OPENWRT.2305.2.sh

--- OPENWRT.2305.2.sh
+++ OPENWRT.2305.2.sh
@@ -13,7 +13,11 @@
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.99.1/g' package/base-files/files/bin/config_generate
 
-curl -s https://downloads.openwrt.org/releases/23.05.2/targets/x86/64/openwrt-23.05.2-x86-64.manifest | grep kernel | awk '{print $3}' | awk -F- '{print $3}' > vermagic
+#23.05.2
+echo '47964456485559d992fe6f536131fc64' > vermagic
+#curl -s https://downloads.openwrt.org/releases/23.05.2/targets/x86/64/openwrt-23.05.2-x86-64.manifest | grep kernel | awk '{print $3}' | awk -F- '{print $3}' > vermagic
+
+
 
 # Modify BCM57810
 # wget -O target/linux/x86/patches-5.15/993-bnx2x_warpcore_8727_2_5g_sgmii_txfault.patch https://raw.githubusercontent.com/Swiftfrog/OPENWRT-X86_64/main/993-bnx2x_warpcore_8727_2_5g_sgmii_txfault.patch
