b7ce158f3cb3444b81e7885bacf565a0360f8fbb
Albert <jiuzheyanglianxiwo@gmail.com>
2024-03-28 18:05:51 +0800

    Update OPENWRT.2305.2.sh

--- OPENWRT.2305.2.sh
+++ OPENWRT.2305.2.sh
@@ -27,6 +27,6 @@ wget -O package/kernel/linux/Makefile https://raw.githubusercontent.com/Swiftfro
 update golang
 pushd feeds/packages/lang
 #rm -rf golang && svn co https://github.com/openwrt/packages/branches/openwrt-22.03/lang/golang
-#rm -rf golang && svn co https://github.com/coolsnowwolf/packages/trunk/lang/golang
-rm -rf golang && svn co https://github.com/immortalwrt/packages/trunk/lang/golang
+rm -rf golang && svn co https://github.com/coolsnowwolf/packages/trunk/lang/golang
+#rm -rf golang && svn co https://github.com/immortalwrt/packages/trunk/lang/golang
 popd
